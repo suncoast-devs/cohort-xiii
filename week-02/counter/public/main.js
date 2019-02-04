2268e324bac62a53fa495dec5a61825cfab8e239
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-04 12:00:07 -0500

    added js exmaple

--- week-02/counter/public/main.js
+++ week-02/counter/public/main.js
@@ -1,7 +1,38 @@
+let counter = 0
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  document.querySelector('p').textContent = 0
+}
+
+// when a user clicks a button
+const increaseCounter = () => {
+  // i will do the things
+  console.log('button clicked')
+  // increase the varible 'counter' by 1
+  counter = counter + 1
+  // shortcut : counter++
+  console.log(counter)
+  // update the HTML with the new value
+  // hey DOM, find the <p> and set your textContent = counter
+  document.querySelector('p').textContent = counter
+}
+
+const resetCounter = () => {
+  console.log('reseting counter')
+  // set counter = 0
+  counter = 0
+  // update the html
+  console.log('reset counter to ' + counter)
+  document.querySelector('p').textContent = counter
 }
 
 document.addEventListener('DOMContentLoaded', main)
+// go the HTML (DOM)
+// find the button
+// add event listener
+// listen for the 'click' event
+// when the 'click' event happens, run the function increaseCounter
+document
+  .querySelector('.add-one-button')
+  .addEventListener('click', increaseCounter)
+document.querySelector('.reset-button').addEventListener('click', resetCounter)
