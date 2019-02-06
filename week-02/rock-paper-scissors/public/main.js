55b5ecf9cf1e1b306f5d03a77171fbc72ebb0a5a
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-06 11:46:40 -0500

    computer now picks a random action

--- week-02/rock-paper-scissors/public/main.js
+++ week-02/rock-paper-scissors/public/main.js
@@ -1,22 +1,34 @@
 let playerSelection
 
+const computersTurn = () => {
+  console.log('computer turn has started')
+  const possibleSelections = ['rock', 'paper', 'scissors']
+  const pick = Math.floor(Math.random() * possibleSelections.length)
+  const selected = possibleSelections[pick]
+  console.log(selected)
+  document.querySelector('.computer-pick').textContent = selected
+}
+
 // player selected rock
 const playerSelectedRock = () => {
   playerSelection = 'rock'
   console.log(playerSelection)
   document.querySelector('.player-pick').textContent = playerSelection
+  computersTurn()
 }
 
 const playerSelectedPaper = () => {
   playerSelection = 'paper'
   console.log(playerSelection)
   document.querySelector('.player-pick').textContent = playerSelection
+  computersTurn()
 }
 
 const playerSelectedScissors = () => {
   playerSelection = 'scissors'
   console.log(playerSelection)
   document.querySelector('.player-pick').textContent = playerSelection
+  computersTurn()
 }
 
 const main = () => {}
