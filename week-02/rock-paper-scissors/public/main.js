6adf2371094cc4ef763bed95951be8bb83bd2535
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-06 11:32:54 -0500

    player can select an option

--- week-02/rock-paper-scissors/public/main.js
+++ week-02/rock-paper-scissors/public/main.js
@@ -1,7 +1,32 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let playerSelection
+
+const playerSelectedRock = () => {
+  playerSelection = 'rock'
+  console.log(playerSelection)
+  document.querySelector('.player-pick').textContent = playerSelection
 }
 
+const playerSelectedPaper = () => {
+  playerSelection = 'paper'
+  console.log(playerSelection)
+  document.querySelector('.player-pick').textContent = playerSelection
+}
+
+const playerSelectedScissors = () => {
+  playerSelection = 'scissors'
+  console.log(playerSelection)
+  document.querySelector('.player-pick').textContent = playerSelection
+}
+
+const main = () => {}
+
 document.addEventListener('DOMContentLoaded', main)
+document
+  .querySelector('#rockButton')
+  .addEventListener('click', playerSelectedRock)
+document
+  .querySelector('#paperButton')
+  .addEventListener('click', playerSelectedPaper)
+document
+  .querySelector('#scissorsButton')
+  .addEventListener('click', playerSelectedScissors)
