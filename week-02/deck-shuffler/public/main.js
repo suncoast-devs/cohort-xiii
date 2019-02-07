cfa8f0e018a9496828ee8c98d80126b1d2945737
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-07 14:20:26 -0500

    basline

---
+++ week-02/deck-shuffler/public/main.js
@@ -0,0 +1,7 @@
+const main = () => {
+  if (document.querySelector('h1.hello-world')) {
+    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  }
+}
+
+document.addEventListener('DOMContentLoaded', main)
