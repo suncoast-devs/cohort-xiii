cddf0c4dfdfaf881ce93ab0b2542e7c155e44c8f
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-07 10:53:41 -0500

    added comments

--- week-02/dice-roller/public/main.js
+++ week-02/dice-roller/public/main.js
@@ -1,5 +1,9 @@
+// created array to hold all the different types of dices
 const dice = [4, 6, 8, 10, 12, 20, 100]
 
+// created 1 function, that takes a parameter (sides)
+// and generates a random number, and adds that result to the DOM
+// => This was created after we created a very events and saw that all 7 of the dice would have very similar code
 const rollDice = sides => {
   const rand = Math.floor(Math.random() * sides) + 1
   console.log('rolling d' + sides + ', results = ' + rand)
@@ -8,6 +12,8 @@ const rollDice = sides => {
   document.querySelector('.past-rolls').appendChild(rollResult)
 }
 
+// a function that allows us to hook up all the buttons for our dice, based on the array on line 2
+// => this was created after we saw that we repeated several lines of code that looked very similar
 const addDiceButtonEvents = () => {
   for (let i = 0; i < dice.length; i++) {
     document.querySelector('.d' + dice[i]).addEventListener('click', () => {
@@ -16,4 +22,5 @@ const addDiceButtonEvents = () => {
   }
 }
 
+// the one event to add the events once the page is loaded.
 document.addEventListener('DOMContentLoaded', addDiceButtonEvents)
