a28aae7a64f802443014f301a0a587fc8c8c8de7
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-07 10:50:00 -0500

    added abstract example

--- week-02/dice-roller/public/main.js
+++ week-02/dice-roller/public/main.js
@@ -1,3 +1,5 @@
+const dice = [4, 6, 8, 10, 12, 20, 100]
+
 const rollDice = sides => {
   const rand = Math.floor(Math.random() * sides) + 1
   console.log('rolling d' + sides + ', results = ' + rand)
@@ -6,25 +8,12 @@ const rollDice = sides => {
   document.querySelector('.past-rolls').appendChild(rollResult)
 }
 
-document.querySelector('.d4').addEventListener('click', () => {
-  rollDice(4)
-})
-document.querySelector('.d6').addEventListener('click', () => {
-  rollDice(6)
-})
+const addDiceButtonEvents = () => {
+  for (let i = 0; i < dice.length; i++) {
+    document.querySelector('.d' + dice[i]).addEventListener('click', () => {
+      rollDice(dice[i])
+    })
+  }
+}
 
-document.querySelector('.d8').addEventListener('click', () => {
-  rollDice(8)
-})
-document.querySelector('.d10').addEventListener('click', () => {
-  rollDice(10)
-})
-document.querySelector('.d12').addEventListener('click', () => {
-  rollDice(12)
-})
-document.querySelector('.d20').addEventListener('click', () => {
-  rollDice(20)
-})
-document.querySelector('.d100').addEventListener('click', () => {
-  rollDice(100)
-})
+document.addEventListener('DOMContentLoaded', addDiceButtonEvents)
