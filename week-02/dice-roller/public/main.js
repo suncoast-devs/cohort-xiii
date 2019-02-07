94f2cf62cdfbb1c5831a35ec34ca427574460020
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-07 10:37:06 -0500

    MVP, basic abstraction

--- week-02/dice-roller/public/main.js
+++ week-02/dice-roller/public/main.js
@@ -1,7 +1,30 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const rollDice = sides => {
+  const rand = Math.floor(Math.random() * sides) + 1
+  console.log('rolling d' + sides + ', results = ' + rand)
+  const rollResult = document.createElement('li')
+  rollResult.textContent = rand
+  document.querySelector('.past-rolls').appendChild(rollResult)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.d4').addEventListener('click', () => {
+  rollDice(4)
+})
+document.querySelector('.d6').addEventListener('click', () => {
+  rollDice(6)
+})
+
+document.querySelector('.d8').addEventListener('click', () => {
+  rollDice(8)
+})
+document.querySelector('.d10').addEventListener('click', () => {
+  rollDice(10)
+})
+document.querySelector('.d12').addEventListener('click', () => {
+  rollDice(12)
+})
+document.querySelector('.d20').addEventListener('click', () => {
+  rollDice(20)
+})
+document.querySelector('.d100').addEventListener('click', () => {
+  rollDice(100)
+})
