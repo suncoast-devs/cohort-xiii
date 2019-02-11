1b925921e8e83329261f2f422e228089d84ff332
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-11 12:03:46 -0500

    showing player score and hadn

--- week-03/blackjack-review/public/main.js
+++ week-03/blackjack-review/public/main.js
@@ -57,6 +57,12 @@ const ranks = [
   }
 ]
 
+const player = {
+  hand: [],
+  wins: 0,
+  losses: 0
+}
+
 const createDeck = () => {
   deck = []
   suits.forEach(suit => {
@@ -85,9 +91,73 @@ const shuffleDeck = () => {
   console.log('shuffled deck', deck)
 }
 
+const dealCardToPlayer = () => {
+  // .shift to remove the first card from deck
+  const nextCard = deck.shift()
+  // .push to add to the player hand
+  player.hand.push(nextCard)
+}
+
+const displayPlayerHand = () => {
+  // remove all the cards in the existing ul
+  const parent = document.querySelector('.player-hand')
+  while (parent.firstChild) {
+    parent.firstChild.remove()
+  }
+  // document.querySelector('.player-hand').textContent = ''
+  const lis = player.hand.map(card => {
+    const img = document.createElement('img')
+    img.src = '/images/' + card.imageUrl + '.svg'
+    const li = document.createElement('li')
+    li.appendChild(img)
+    return li
+  })
+  lis.forEach(li => {
+    document.querySelector('.player-hand').appendChild(li)
+  })
+}
+
+const displayPlayerTotal = () => {
+  // sum up all the cards in a players hand
+  // reduce
+  // const playerScore = player.hand.reduce((total, card) => {
+  //   total += card.value
+  //   return total
+  // }, 0)
+
+  // using forEach
+  // let playerScore = 0
+  // player.hand.forEach(card => {
+  //   playerScore += card.value
+  // })
+
+  // for loop
+  let playerScore = 0
+  for (let i = 0; i < player.hand.length; i++) {
+    playerScore += player.hand[i].value
+  }
+
+  document.querySelector('.player-score').textContent = playerScore
+}
+
+const startGame = () => {
+  // deal 2 cards to player (be sure to update the deck)
+  dealCardToPlayer()
+  dealCardToPlayer()
+  console.log(player)
+  // show player their cards (and their points)
+  // display cards as lis
+  displayPlayerHand()
+  displayPlayerTotal()
+  // get the sum
+  // deal 2 cards to dealer
+  // show only 1 dealer card
+}
+
 const main = () => {
   createDeck()
   shuffleDeck()
+  startGame()
 }
 
 document.addEventListener('DOMContentLoaded', main)
