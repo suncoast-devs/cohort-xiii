c062f97d0e5973b36de3c895cd5c04b75531afe0
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-12 11:41:11 -0500

    basic fetch exmaple

--- week-03/joke-api/public/main.js
+++ week-03/joke-api/public/main.js
@@ -1,7 +1,26 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let currentJoke = {}
+
+const getJoke = () => {
+  // send the request
+  fetch('https://official-joke-api.appspot.com/random_joke')
+    // getting the response back
+    .then(resp => {
+      return resp.json()
+    })
+    // opening the response, joke is the actual data that we want
+    .then(joke => {
+      console.log(joke)
+      currentJoke = joke
+      document.querySelector('.joke').textContent = joke.setup
+      document.querySelector('.punchline').textContent = ''
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const revealPunchline = () => {
+  document.querySelector('.punchline').textContent = currentJoke.punchline
+}
+
+document
+  .querySelector('.reveal-punchline')
+  .addEventListener('click', revealPunchline)
+document.querySelector('.get-joke-button').addEventListener('click', getJoke)
