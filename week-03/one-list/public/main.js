899becaca3acb8a49212a64d4cafef9b02d983fd
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-14 16:23:53 -0500

    added comments and notes from with me at 3

--- week-03/one-list/public/main.js
+++ week-03/one-list/public/main.js
@@ -1,19 +1,21 @@
 const API_URL = `https://one-list-api.herokuapp.com/items?access_token=cohort-xiii`
 
 // Classes
+// build a class to represent a list item
 class ListItem {
-  constructor(data) {
+  constructor (data) {
     this.text = data.text
     this.id = data.id
   }
 }
 
+// build a class to represent the list
 class TodoList {
-  constructor() {
+  constructor () {
     this.list = []
   }
 
-  render() {
+  render () {
     // this is where we update the HTML
     const parent = document.querySelector('.todo-list')
     // remove all the items
@@ -26,6 +28,9 @@ class TodoList {
         // create the HTML for an list item
         const span = document.createElement('span')
         span.textContent = item.text
+        span.addEventListener('click', () => {
+          markItemAsComplete(item)
+        })
         if (item.complete) {
           span.classList.add('completed-item')
         }
@@ -34,11 +39,11 @@ class TodoList {
         icon.classList.add('fa-trash-alt')
         const btn = document.createElement('button')
         btn.appendChild(icon)
+        btn.addEventListener('click', () => {
+          deleteItem(item)
+        })
         const li = document.createElement('li')
         const section = document.createElement('section')
-        section.addEventListener('click', () => {
-          markItemAsComplete(item)
-        })
         section.appendChild(span)
         section.appendChild(btn)
         li.appendChild(section)
@@ -50,19 +55,40 @@ class TodoList {
   }
 }
 // Events
+
+// page load event
 const main = () => {
   // do a fetch to API
-  // diplay my list items
   fetch(API_URL)
     .then(resp => resp.json())
     .then(list => {
       TODO_LIST.list = list
+      // diplay my list items using a render function
       TODO_LIST.render()
     })
 }
 
+// delete an item from the API/UI
+const deleteItem = itemToDelete => {
+  // calling the API to delete the item
+  fetch(
+    `https://one-list-api.herokuapp.com/items/${
+      itemToDelete.id
+    }?access_token=cohort-xiii`,
+    {
+      method: 'DELETE'
+    }
+  ).then(resp => {
+    // removing the item from object, and the HTML
+    TODO_LIST.list = TODO_LIST.list.filter(item => item.id !== itemToDelete.id)
+    TODO_LIST.render()
+  })
+}
+
 const markItemAsComplete = item => {
+  // toggle an item's completin status, this is so the itsm status can be toggled by the user
   item.complete = !item.complete
+  // update on the server
   fetch(
     `https://one-list-api.herokuapp.com/items/${
       item.id
@@ -82,31 +108,47 @@ const markItemAsComplete = item => {
     if (resp.status >= 300) {
       console.log('something went wrong')
     } else {
+      // Update the UI on success
       TODO_LIST.render()
     }
   })
 }
 
+// create  mesthod that shows a message then hides it after 3 seconds
+const showErrorMessage = message => {
+  const output = document.querySelector('.output')
+  output.textContent = message
+  setTimeout(() => {
+    output.textContent = ''
+  }, 3000)
+}
+
+// adds an item to the API
 const addItem = event => {
   event.preventDefault()
   const newItemToAdd = document.querySelector('#newItem').value
-  fetch(API_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      item: {
-        text: newItemToAdd
-      }
+  // only adds the new item if the item is something (a truthy value)
+  if (newItemToAdd) {
+    fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        item: {
+          text: newItemToAdd
+        }
+      })
     })
-  })
-    .then(resp => resp.json())
-    .then(listItem => {
-      TODO_LIST.list.push(new ListItem(listItem))
-      // update the list in the HTML
-      TODO_LIST.render()
-    })
+      .then(resp => resp.json())
+      .then(listItem => {
+        TODO_LIST.list.push(new ListItem(listItem))
+        // update the list in the HTML
+        TODO_LIST.render()
+      })
+  } else {
+    showErrorMessage('Task cannot be blank')
+  }
 }
 
 const TODO_LIST = new TodoList()
