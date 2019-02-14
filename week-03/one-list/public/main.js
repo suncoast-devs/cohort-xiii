0c6c048eea024c178f7bfddacb5ede02962a641c
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-14 11:11:09 -0500

    adding items to list

--- week-03/one-list/public/main.js
+++ week-03/one-list/public/main.js
@@ -1,7 +1,86 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const API_URL = `https://one-list-api.herokuapp.com/items?access_token=cohort-xiii`
+
+// Classes
+class ListItem {
+  constructor(data) {
+    this.text = data.text
+    this.id = data.id
   }
 }
 
+class TodoList {
+  constructor() {
+    this.list = []
+  }
+
+  render() {
+    // this is where we update the HTML
+    const parent = document.querySelector('.todo-list')
+    // remove all the items
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild)
+    }
+    // add all the items from the property this.list
+    this.list
+      .map(item => {
+        // create the HTML for an list item
+        const span = document.createElement('span')
+        console.log(item)
+        span.textContent = item.text
+        const icon = document.createElement('i')
+        icon.classList.add('far')
+        icon.classList.add('fa-trash-alt')
+        const btn = document.createElement('button')
+        btn.appendChild(icon)
+        const li = document.createElement('li')
+        const section = document.createElement('section')
+        section.appendChild(span)
+        section.appendChild(btn)
+        li.appendChild(section)
+        return li
+      })
+      .forEach(li => {
+        parent.appendChild(li)
+      })
+  }
+}
+// Events
+const main = () => {
+  // do a fetch to API
+  // diplay my list items
+  fetch(API_URL)
+    .then(resp => resp.json())
+    .then(list => {
+      console.log(list)
+      TODO_LIST.list = list
+      TODO_LIST.render()
+    })
+}
+
+const addItem = event => {
+  event.preventDefault()
+  const newItemToAdd = document.querySelector('#newItem').value
+  console.log('adding item', newItemToAdd)
+  fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      item: {
+        text: newItemToAdd
+      }
+    })
+  })
+    .then(resp => resp.json())
+    .then(listItem => {
+      console.log(listItem)
+      TODO_LIST.list.push(new ListItem(listItem))
+      // update the list in the HTML
+      TODO_LIST.render()
+    })
+}
+
+const TODO_LIST = new TodoList()
+document.querySelector('.add-form').addEventListener('submit', addItem)
 document.addEventListener('DOMContentLoaded', main)
