72e2d9280ffcdb9fbd28d2c199ef69811d4ca407
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-19 12:11:06 -0500

    added react intro

--- week-04/intro-to-react/src/App.js
+++ week-04/intro-to-react/src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+
+import WelcomeMessage from './components/WelcomeMessage'
+
+const heros = [
+  'Superman',
+  'Batman',
+  'Wonder Woman',
+  'Black Panther',
+  'Doctor Strange',
+  'Cat Woman',
+  'Flash',
+  'Shazam',
+  'Capt Marvel',
+  'Batman'
+]
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <section>
+        Hello, Welcome to react
+        {heros.map(hero => {
+          return <WelcomeMessage username={hero} />
+        })}
+      </section>
+    )
   }
 }
 
