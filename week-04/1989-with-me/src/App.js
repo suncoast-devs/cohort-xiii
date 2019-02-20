be4ae57429d8e02f708d04f0deddddb9c80f03bd
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-20 15:16:23 -0500

    added image example for 1989

---
+++ week-04/1989-with-me/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import HelloWorld from './components/HelloWorld'
+
+const myMovie = {
+  vote_count: 4300,
+  id: 89,
+  video: false,
+  vote_average: 7.7,
+  title: 'Indiana Jones and the Last Crusade',
+  popularity: 18.43,
+  poster_path: '/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg',
+  original_language: 'en',
+  original_title: 'Indiana Jones and the Last Crusade',
+  genre_ids: [12, 28],
+  backdrop_path: '/vfvVuu1JdnEGcyZUj7VHrhhbeMj.jpg',
+  adult: false,
+  overview:
+    "When Dr. Henry Jones Sr. suddenly goes missing while pursuing the Holy Grail, eminent archaeologist Indiana must team up with Marcus Brody, Sallah and Elsa Schneider to follow in his father's footsteps and stop the Nazis from recovering the power of eternal life.",
+  release_date: '1989-05-24'
+}
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <img
+          src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${
+            myMovie.poster_path
+          }`}
+        />
+      </section>
+    )
+  }
+}
+
+export default App
