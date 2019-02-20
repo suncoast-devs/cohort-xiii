6aabfa745b804654e5b2a8e97926fb71b452932b
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-20 15:59:52 -0500

    added some image nonsense

--- week-04/1989-with-me/src/App.js
+++ week-04/1989-with-me/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
+import FontAwesome from 'react-fontawesome'
+
 const myMovie = {
   vote_count: 4300,
   id: 89,
@@ -8,7 +10,7 @@ const myMovie = {
   vote_average: 7.7,
   title: 'Indiana Jones and the Last Crusade',
   popularity: 18.43,
-  poster_path: '/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg',
+  poster_path: './images/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg',
   original_language: 'en',
   original_title: 'Indiana Jones and the Last Crusade',
   genre_ids: [12, 28],
@@ -27,6 +29,13 @@ class App extends Component {
           src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${
             myMovie.poster_path
           }`}
+        />
+        <FontAwesome
+          className="super-crazy-colors"
+          name="rocket"
+          size="2x"
+          spin
+          style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
         />
       </section>
     )
