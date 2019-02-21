24d1ebe6fa2887e3373e384c78602ed05a0ddb55
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-21 15:48:55 -0500

    added list example

--- week-04/list-example/src/App.js
+++ week-04/list-example/src/App.js
@@ -2,8 +2,28 @@ import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
 
 class App extends Component {
+  state = {
+    numbers: []
+  }
+
+  addNumberToList = () => {
+    const random = Math.ceil(Math.random() * 6)
+    this.setState({
+      numbers: this.state.numbers.concat(random)
+    })
+  }
+
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <button onClick={this.addNumberToList}>Click me!</button>
+        <ul>
+          {this.state.numbers.map(number => {
+            return <li>{number}</li>
+          })}
+        </ul>
+      </>
+    )
   }
 }
 
