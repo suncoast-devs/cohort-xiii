eda39bff4063c52a0a70a26231e2241b04add2b8
Mark Dewey <mark.t.dewey@gmail.com>
2019-02-26 09:42:16 -0500

    ready for memory game

--- week-05/nested-maps-example/src/App.js
+++ week-05/nested-maps-example/src/App.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
-
+import axios from 'axios'
 class App extends Component {
+  state = {
+    game: [
+      ['💣', '🚩', ' ', '💣', '🚩', ' ', '💣', '🚩', ' '],
+      [' ', '💣', 6, ' ', '💣', 6, ' ', '💣', 6],
+      [7, 8, 9, 7, 8, 9, 7, 8, 9],
+      ['💣', '🚩', ' ', '💣', '🚩', ' ', '💣', '🚩', ' '],
+      [' ', '💣', 6, ' ', '💣', 6, ' ', '💣', 6],
+      [7, 8, 9, 7, 8, 9, 7, 8, 9],
+      ['💣', '🚩', ' ', '💣', '🚩', ' ', '💣', '🚩', ' '],
+      [' ', '💣', 6, ' ', '💣', 6, ' ', '💣', 6],
+      [7, 8, 9, 7, 8, 9, 7, 8, 9],
+      ['💣', '🚩', ' ', '💣', '🚩', ' ', '💣', '🚩', ' '],
+      [' ', '💣', 6, ' ', '💣', 6, ' ', '💣', 6],
+      [7, 8, 9, 7, 8, 9, 7, 8, 9]
+    ]
+  }
+
+  componentDidMount() {
+    // axios goes here.....
+    axios
+      .post('https://minesweeper-api.herokuapp.com/games', {
+        difficulty: 0
+      })
+      .then(resp => {
+        console.log({ resp })
+        this.setState({
+          game: resp.data.board
+        })
+      })
+  }
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <table>
+          <tbody>
+            {this.state.game.map(row => {
+              return (
+                <tr>
+                  {row.map(col => {
+                    return (
+                      <td>
+                        <button>{col} </button>
+                      </td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </>
+    )
   }
 }
 
