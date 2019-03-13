48deb0ca84816ba2fbd0ededb6e4af698b0d534c
Mark Dewey <mark.t.dewey@gmail.com>
2019-03-13 15:20:08 -0400

    added number guesser base

---
+++ dotnet/week-07/NumberGuesser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var stillGuessing = true;
+      var counter = 0;
+      while (stillGuessing)
+      {
+        counter++;
+        // figure out the next guess
+        // ask the user if the guess was correct
+        Console.WriteLine("Is your number 65?");
+        var input = Console.ReadLine();
+        if (input == "yes")
+        {
+          stillGuessing = false;
+        }
+      }
+      Console.WriteLine($"Hey, I guess a total of {counter} times and your number was Y");
+
+
+    }
+  }
+}
