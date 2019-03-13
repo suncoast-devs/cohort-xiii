155c40b9231399c6636f2c90cc159e5ba55405a5
Mark Dewey <mark.t.dewey@gmail.com>
2019-03-13 11:12:43 -0400

    added gitignore

---
+++ dotnet/week-07/HelloWorld/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome!, What is your name?");
+      var name = Console.ReadLine();
+      Console.WriteLine($"Hello, {name}. what is your favorite number");
+      var favoriteNumber = int.Parse(Console.ReadLine());
+
+
+      // string 
+      var aString = "this is a string";
+      // char 
+      var aCharacter = 'x';
+
+      // Numbers
+      var number = -10;
+      //   number = null;
+      var aNumberWithDecimalPoints = 10.5m;
+
+      //bool 
+
+      // byte
+
+
+      // dynamic!?!?! Don't use this!!!
+      dynamic thisIsAThing = "hello";
+      thisIsAThing = 42;
+
+      // gitignore
+
+
+      // if
+
+      // arrays/list
+
+      // loops
+
+      // methods
+
+      // dev env set up
+    }
+  }
+}
