49280b56e4198a58ad37f2a35dd046b7f4fac271
Mark Dewey <mark.t.dewey@gmail.com>
2019-03-18 11:48:26 -0400

    ready for multiple tables

--- dotnet/week-08/IntroToOrms/Program.cs
+++ dotnet/week-08/IntroToOrms/Program.cs
@@ -1,12 +1,109 @@
 ﻿using System;
+using System.Linq;
 
 namespace IntroToOrms
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Lets do some team building!");
+
+      // we need to create a new instance of our database context class
+      var db = new IntroToOrmsContext();
+
+      // create (insert)
+      // INSERT INTO Teams (Mascot, Coach.....) VALUES('Wallabyes', 'Jordan'....)
+      var favoriteTeam = new Team
+      {
+        Mascot = "Wallabyes",
+        Coach = "Jordan",
+        ChampionsWon = 3,
+        AverageTicketPrice = 3.50,
+        FoundedDate = DateTime.Now,
+        City = "Sydney"
+      };
+      var favoriteTeam2 = new Team
+      {
+        Mascot = "Patriots",
+        Coach = "Silvana",
+        ChampionsWon = 3,
+        AverageTicketPrice = 3.50,
+        FoundedDate = DateTime.Now,
+        City = "Sydney"
+      };
+      var favoriteTeam4 = new Team
+      {
+        Mascot = "Eagles",
+        Coach = "Andrews",
+        ChampionsWon = 3,
+        AverageTicketPrice = 3.50,
+        FoundedDate = DateTime.Now,
+        City = "Sydney"
+      };
+      var favoriteTeam3 = new Team
+      {
+        Mascot = "Cowbots",
+        Coach = "Manny",
+        ChampionsWon = 3,
+        AverageTicketPrice = 3.50,
+        FoundedDate = DateTime.Now,
+        City = "Sydney"
+      };
+
+
+
+      //   db.Teams.Add(favoriteTeam);
+      //   db.Teams.Add(favoriteTeam2);
+      //   db.Teams.Add(favoriteTeam3);
+      //   db.Teams.Add(favoriteTeam4);
+      db.SaveChanges();
+
+
+
+      // select 
+      // SELECT * FROM Teams WHERE Mascot = 'Patriots' OR Mascot = 'Eagles'
+      var thoseTeams = db.Teams.Where(team => team.Mascot == "Patriots" || team.Mascot == "Eagles");
+    )
+      foreach (var team in thoseTeams)
+      {
+        Console.WriteLine(team.City + " " + team.Mascot);
+      }
+
+      // update
+      // find the item we want to update
+      // SELECT TOP(1) * FROM Teams WHERE Mascot = 'Eagles'
+      // if  nothing is found, then return 'null'
+      var eagles = db.Teams.FirstOrDefault(team => team.Mascot == "Eagles");
+      // update it
+      if (eagles != null)
+      {
+        eagles.City = "philly";
+        eagles.ChampionsWon = 1;
+        eagles.AverageTicketPrice = 100.10;
+      }
+      // save the changes 
+      db.SaveChanges();
+
+      // update mutiple
+      var teamsToUpdate = db.Teams.Where(teams => teams.Mascot != "Eagles");
+      foreach (var team in teamsToUpdate)
+      {
+        team.ChampionsWon = 0;
+      }
+      db.SaveChanges();
+
+      // delete
+      // DELETE FROM Teams WHERE Id = 2    
+      // find the thing to delete
+      var thatTeam = db.Teams.FirstOrDefault(team => team.Id == 2);
+      if (thatTeam != null)
+      {
+        // delete it
+        db.Teams.Remove(thatTeam);
+      }
+      // save the changes
+      db.SaveChanges();
     }
+  }
 }
