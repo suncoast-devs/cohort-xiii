75c9e1cdf5caf3b61368f826377090e3538d5637
Mark Dewey <mark.t.dewey@gmail.com>
2019-03-19 11:00:54 -0400

    ready for lecture, with basic cmd interface

--- dotnet/week-08/HackerNews/Program.cs
+++ dotnet/week-08/HackerNews/Program.cs
@@ -7,7 +7,12 @@ namespace HackerNews
     static void Main(string[] args)
     {
 
-      while (true)
+      Console.WriteLine("Welcome to cmd hacker news, who are you?");
+      var username = Console.ReadLine();
+      // TODO: create or get user 
+      var reading = true;
+
+      while (reading)
       {
 
         Console.WriteLine("Welcome to cmd hacker news");
@@ -15,12 +20,12 @@ namespace HackerNews
         Console.WriteLine("see the most (recent)");
         Console.WriteLine("see the (top)");
         Console.WriteLine("(view {id}) article ");
-        Console.WriteLine("(upvote {id}) article ");
-        Console.WriteLine("Add a new article");
+        Console.WriteLine("(favorite {id}) article ");
+        Console.WriteLine("view my (favorites) ");
+        Console.WriteLine("(add) a new article");
+        Console.WriteLine("(exit)");
         var input = Console.ReadLine();
         Console.Clear();
-        Console.WriteLine($"You picked {input}");
-
         if (input == "recent")
         {
           Console.WriteLine("Recent Articles");
@@ -32,11 +37,17 @@ namespace HackerNews
           Console.WriteLine("=========================");
 
         }
-        else if (input.Contains("upvote"))
+        else if (input == "favorites")
+        {
+          Console.WriteLine("Viewing your favorites");
+          Console.WriteLine("=========================");
+
+        }
+        else if (input.Contains("favorite"))
         {
           if (int.TryParse(input.Split(' ')[1], out int articleId))
           {
-            Console.WriteLine($"up voting article {articleId}");
+            Console.WriteLine($"favoriting article {articleId}");
           }
         }
         else if (input.Contains("see"))
@@ -50,11 +61,14 @@ namespace HackerNews
         {
           Console.WriteLine("Adding an article...");
         }
+        else if (input == "exit")
+        {
+          reading = false;
+          Console.WriteLine("Bye bye!");
+        }
         else
         {
           Console.WriteLine("I am not sure what you mean, try again");
-
-
         }
         Console.WriteLine();
         Console.WriteLine();
