3fd5cc3b44f653c717f1a40aa39fcd748ce08ac5
Mark Dewey <mark.t.dewey@gmail.com>
2019-03-20 16:13:27 -0400

    added the stubbed endpoints for getting all/one movies and creating movies

--- dotnet/week-08/MovieApi/Controllers/MoviesController.cs
+++ dotnet/week-08/MovieApi/Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
 
 namespace movieapi.Controllers
 {
@@ -8,7 +10,28 @@ namespace movieapi.Controllers
   public class MoviesController : ControllerBase
   {
 
+    [HttpGet]
+    public ActionResult<IEnumerable<Movie>> GetAllMovies()
+    {
+      // TODO: query the database
+      // return the results
+      return new List<Movie> { new Movie { Title = "Jaws" }, new Movie { Title = "GoodFellas" } };
+    }
 
+    [HttpGet("{id}")]
+    public ActionResult<Movie> GetOneMovie(int id)
+    {
+      return new Movie { Title = "E.T.", Id = id };
+    }
+
+    [HttpPost]
+    public ActionResult<Movie> CreateMovie([FromBody] Movie movieToAdd)
+    {
+      return new Movie
+      {
+        Title = movieToAdd.Title
+      };
+    }
 
   }
 }
---
+++ dotnet/week-08/MovieApi/Models/Movie.cs
@@ -0,0 +1,10 @@
+namespace MovieApi.Models
+{
+  public class Movie
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Rating { get; set; }
+    public string Director { get; set; }
+  }
+}
