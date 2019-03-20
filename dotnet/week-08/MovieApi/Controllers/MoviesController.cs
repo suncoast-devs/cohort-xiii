8e7b7abff00f27a7da4717b3ebffc22b0e49d289
Mark Dewey <mark.t.dewey@gmail.com>
2019-03-20 15:44:13 -0400

    added movie controller

---
+++ dotnet/week-08/MovieApi/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace movieapi.Controllers
+{
+
+  [Route("api/[controller]")]
+  [ApiController]
+  public class MoviesController : ControllerBase
+  {
+
+
+
+  }
+}
