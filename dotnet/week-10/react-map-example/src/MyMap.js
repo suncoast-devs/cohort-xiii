60b3bbc9404e09902812b1e80ad22a292b909e8d
Mark Dewey <mark.t.dewey@gmail.com>
2019-04-03 10:22:56 -0400

    added react map stuff

--- dotnet/week-10/react-map-example/src/MyMap.js
+++ dotnet/week-10/react-map-example/src/MyMap.js
@@ -1,8 +1,52 @@
 import React, { Component } from 'react'
-
+import ReactMapGL, { Marker } from 'react-map-gl'
 class MyMap extends Component {
+  state = {
+    viewport: {
+      width: 1000,
+      height: 1000,
+      latitude: 28.1,
+      longitude: -82.6,
+      zoom: 8
+    },
+    userLocation: {
+      lat: 0,
+      lng: 0
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords
+      console.log({ latitude, longitude })
+      const oldViewport = { ...this.state.viewport }
+      oldViewport.latitude = latitude
+      oldViewport.longitude = longitude
+      oldViewport.zoom = 15
+      this.setState({
+        userLocation: { lat: latitude, lng: longitude },
+        viewport: oldViewport
+      })
+    })
+  }
+
   render() {
-    return <div>my map goes here</div>
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        mapboxApiAccessToken={
+          'pk.eyJ1IjoiZGV3c2VwaCIsImEiOiJjanRrNzQwYTYwMHdjM3lwNnh2am05ejc0In0._LCOTuYCw-eRKDvUut2TxQ'
+        }
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+        <Marker
+          longitude={this.state.userLocation.lng}
+          latitude={this.state.userLocation.lat}
+        >
+          🚩
+        </Marker>
+      </ReactMapGL>
+    )
   }
 }
 
