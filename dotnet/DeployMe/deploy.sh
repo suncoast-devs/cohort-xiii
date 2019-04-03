54e373e58c568716189003015ec3d2c060cd6d7e
Mark Dewey <mark.t.dewey@gmail.com>
2019-04-03 16:52:32 -0400

    added deployment

--- dotnet/DeployMe/deploy.sh
+++ dotnet/DeployMe/deploy.sh
@@ -2,13 +2,13 @@ dotnet publish -c Release
 
 cp dockerfile ./bin/release/netcoreapp2.2/publish
 
-docker build -t sdg-template-image ./bin/release/netcoreapp2.2/publish
+docker build -t deploy-me-image ./bin/release/netcoreapp2.2/publish
 
-docker tag sdg-template-image registry.heroku.com/heroku-web-app/web
+docker tag deploy-me-image registry.heroku.com/cohort-xiii-sample-app/web
 
-docker push registry.heroku.com/heroku-web-app/web
+docker push registry.heroku.com/cohort-xiii-sample-app/web
 
-heroku container:release web -a heroku-web-app
+heroku container:release web -a cohort-xiii-sample-app
 
 # sudo chmod 755 deploy.sh
 # ./deploy.sh
